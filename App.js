7fc749e41e2c41995e1758445684591940c6142c
Nikhil Shaw <69413813+nikhilshaw3@users.noreply.github.com>
2022-02-27 13:06:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Main from "./screens/main"
+
+export default function App() {
+  return (
+    <Main/>
+  );
+}
